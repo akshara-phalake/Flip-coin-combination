189dc56deefdf71896c6fd947716943e02013874
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 19:39:17 +0530

    Triplet Commbination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -76,5 +76,85 @@ echo " Percentage of tail_tail combination:" $percentageofTailTail"%"
 
 }
 
+#triplet combination
+tripletcombination ( ) {
+echo "      TRIPLET COMBINATION     "
+coin[headheadhead]=0
+coin[headheadtail]=0
+coin[headtailhead]=0
+coin[headtailtail]=0
+coin[tailheadhead]=0
+coin[tailheadtail]=0
+coin[tailtailhead]=0
+coin[tailtailtail]=0
+
+for (( i=0; i<$loop; i++ ))
+do
+firstcoinflip=$((RANDOM%2))
+secondcoinflip=$((RANDOM%2))
+thirdcoinflip=$((RANDOM%2))
+
+if [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 0 ]
+then
+        coin[headheadhead]=$(( ${coin[headheadhead]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 1 ]
+then
+		  coin[headheadtail]=$(( ${coin[headheadtail]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 0  ]
+then
+        coin[headtailhead]=$(( ${coin[headtailhead]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 1  ]
+then
+        coin[headtailtail]=$(( ${coin[headtailtail]}+1 ))
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 0 ]
+then
+        coin[tailheadhead]=$(( ${coin[tailheadhead]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 1 ]
+then
+        coin[tailheadtail]=$(( ${coin[tailheadtail]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 0  ]
+then
+        coin[tailtailhead]=$(( ${coin[tailtailhead]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 1  ]
+then
+        coin[tailtailtail]=$(( ${coin[tailtailtail]}+1 ))
+fi
+done
+
+echo " Number of head_head_head combination flips" ${coin[headheadhead]}
+echo " Number of head_head_tail combination flips" ${coin[headheadtail]}
+echo " Number of head_tail_head combination flips" ${coin[headtailhead]}
+echo " Number of head_tail_tail combination flips" ${coin[headtailtail]}
+echo " Number of tail_head_head combination flips" ${coin[tailheadhead]}
+echo " Number of tail_head_tail combination flips" ${coin[tailheadtail]}
+echo " Number of tail_tail_head combination flips" ${coin[tailtailhead]}
+echo " Number of tail_tail_tail combination flips" ${coin[tailtailtail]}
+
+sumofcombinations=$(( ${coin[headheadhead]}+${coin[headheadtail]}+${coin[headtailhead]}+${coin[headtailtail]}+ ${coin[tailheadhead]}+${coin[tailheadtail]}+${coin[tailtailhead]}+${coin[tailtailtail]} ))
+percentageofHeadHeadHead=$(( (${coin[headheadhead]}*100)/$sumofcombinations ))
+percentageofHeadHeadTail=$(( (${coin[headheadtail]}*100)/$sumofcombinations ))
+percentageofHeadTailHead=$(( (${coin[headtailhead]}*100)/$sumofcombinations ))
+percentageofHeadTailTail=$(( (${coin[headtailtail]}*100)/$sumofcombinations ))
+percentageofTailHeadHead=$(( (${coin[tailheadhead]}*100)/$sumofcombinations ))
+percentageofTailHeadTail=$(( (${coin[tailheadtail]}*100)/$sumofcombinations ))
+percentageofTailTailHead=$(( (${coin[tailtailhead]}*100)/$sumofcombinations ))
+percentageofTailTailTail=$(( (${coin[tailtailtail]}*100)/$sumofcombinations ))
+
+echo " Percentage of head_head_head combination:" $percentageofHeadHeadHead"%"
+echo " Percentage of head_head_tail combination:" $percentageofHeadHeadTail"%"
+echo " Percentage of head_tail_head combination:" $percentageofHeadTailHead"%"
+echo " Percentage of head_tail_tail combination:" $percentageofHeadTailTail"%"
+echo " Percentage of tail_head_head combination:" $percentageofTailHeadHead"%"
+echo " Percentage of tail_head_tail combination:" $percentageofTailHeadTail"%"
+echo " Percentage of tail_tail_head combination:" $percentageofTailTailHead"%"
+echo " Percentage of tail_tail_tail combination:" $percentageofTailTailTail"%"
+}
 singletcombination
 doubletcombination
+tripletcombination
