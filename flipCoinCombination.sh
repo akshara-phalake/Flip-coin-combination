4203e06a81b2cffb3ecc69b19bf8d0475c459ace
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 19:28:36 +0530

    Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "		SINGLET COMBINATION		"
 loop=20
 declare -A coin
 #singlet combination
 singletcombination ( ) {
-
+echo "      SINGLET COMBINATION     "
 coin[heads]=0
 coin[tails]=0
 
@@ -30,4 +29,52 @@ echo "Percentage of heads:" $percentageofHeads"%"
 echo "Percentage of tails:" $percentageofTails"%"
 
 }
+#doublet combination
+doubletcombination ( ) {
+echo "      DOUBLET COMBINATION     "
+
+coin[headhead]=0
+coin[headtail]=0
+coin[tailhead]=0
+coin[tailtail]=0
+
+for (( i=0; i<$loop; i++ ))
+do
+firstcoinflip=$((RANDOM%2))
+secondcoinflip=$((RANDOM%2))
+
+if [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 0 ]
+then
+	coin[headhead]=$(( ${coin[headhead]}+1 ))
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 1 ]
+then
+	coin[headtail]=$(( ${coin[headtail]}+1 ))
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 0 ]
+then
+   coin[tailhead]=$(( ${coin[tailhead]}+1 ))
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 1 ]
+then
+    coin[tailtail]=$(( ${coin[tailtail]}+1 ))
+fi
+done
+
+echo " Number of head_head combination flips" ${coin[headhead]}
+echo " Number of head_tail combination flips" ${coin[headtail]}
+echo " Number of tail_head combination flips" ${coin[tailhead]}
+echo " Number of tail_tail combination flips" ${coin[tailtail]}
+
+sumofcombinations=$(( ${coin[headhead]}+${coin[headtail]}+${coin[tailhead]}+${coin[tailtail]} ))
+percentageofHeadHead=$(( (${coin[headhead]}*100)/$sumofcombinations ))
+percentageofHeadTail=$(( (${coin[headtail]}*100)/$sumofcombinations ))
+percentageofTailHead=$(( (${coin[tailhead]}*100)/$sumofcombinations ))
+percentageofTailTail=$(( (${coin[tailtail]}*100)/$sumofcombinations ))
+
+echo " Percentage of head_head combination:" $percentageofHeadHead"%"
+echo " Percentage of head_tail combination:" $percentageofHeadTail"%"
+echo " Percentage of tail_head combination:" $percentageofTailHead"%"
+echo " Percentage of tail_tail combination:" $percentageofTailTail"%"
+
+}
+
 singletcombination
+doubletcombination
